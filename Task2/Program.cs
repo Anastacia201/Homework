e6d2d6ee73b1332bfc2a23ef2244edabc0c720eb
Anastacia <nastia.peresypkina2000@gmail.com>
2022-12-27 12:59:36 +0400

    Added tasks

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Enter num: ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 0;
+int temp = 0;
+
+for(;N>0;N/=10)
+{
+    temp = temp + N%10;
+    i++;
+}
+
+Console.WriteLine($"{temp} ");
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+Console.WriteLine("Enter number1: ");
+int N1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number1: ");
+int N2 = int.Parse(Console.ReadLine());
+
+int [] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    var rnd = new Random();
+    array[i] = rnd.Next(N1, N2+1);
+
+    System.Console.Write(array[i]+" ");
+}
